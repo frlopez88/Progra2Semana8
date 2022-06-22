ac21c0c4141323f4d57ffabe8d91d1150e6d693c
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2022-06-21 20:55:35 -0600

    Commite de Serializacion de Listas

--- FigurasGeometricas/FigurasGeometricas/ViewModels/ViewModelCirculo.cs
+++ FigurasGeometricas/FigurasGeometricas/ViewModels/ViewModelCirculo.cs
@@ -4,6 +4,7 @@ using Xamarin.Forms;
 using FigurasGeometricas.Models;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
+using System.Collections.ObjectModel;
 
 namespace FigurasGeometricas.ViewModels
 {
@@ -16,16 +17,57 @@ namespace FigurasGeometricas.ViewModels
             CrearCirculo = new Command(
                     ()=>{
 
+
                         C1.calcularArea();
                         C1.calcularPerimetro();
                         C1 = C1;
+                        Circulo tmp = new Circulo()
+                        {
+
+                            radio = C1.radio
+                        };
+
+                        tmp.calcularArea();
+                        tmp.calcularPerimetro();
+
+                        if (lista.Count == 0)
+                        {
+                            lista.Add(tmp);
+                        }else
+                        {
+                            bool existia = false;
+                            for (int i = 0; i < lista.Count; i++)
+                            {
+
+                                if (C1.radio == lista[i].radio)
+                                {
+                                    C1.radio = lista[i].radio;
+                                    C1.area = lista[i].area;
+                                    C1.perimetro  = lista[i].perimetro;
+                                    C1 = C1;
+                                    existia = true;                                     
+                                }
+                            }
+
+                            if (existia == false)
+                            {
+                                lista.Add(tmp);
+                            }
+
+                        }
+
+                        
+
+                        
 
                         BinaryFormatter formatter = new BinaryFormatter();
                         string ruta = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "Circulo.aut");
                         Stream archivo = new FileStream(ruta, FileMode.Create, FileAccess.Write, FileShare.None);
-                        formatter.Serialize(archivo, C1);
+                        formatter.Serialize(archivo, lista);
                         archivo.Close();
 
+
+                        CantidadRegistros = lista.Count;
                     }
                 );
 
@@ -35,9 +77,10 @@ namespace FigurasGeometricas.ViewModels
                 BinaryFormatter formatter = new BinaryFormatter();
                 string ruta = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "Circulo.aut");
                 Stream archivo = new FileStream(ruta, FileMode.Open, FileAccess.Read, FileShare.None);
-                C1 = (Circulo)formatter.Deserialize(archivo);
+                lista = (ObservableCollection<Circulo>)formatter.Deserialize(archivo);
                 archivo.Close();
 
+                CantidadRegistros = lista.Count;
 
             });
 
@@ -60,7 +103,21 @@ namespace FigurasGeometricas.ViewModels
             }
         }
 
-       
+        ObservableCollection<Circulo> lista = new ObservableCollection<Circulo>();
+
+
+        int cantidadRegistros;
+
+        public int CantidadRegistros { get => cantidadRegistros;
+
+            set {
+
+                cantidadRegistros = value;
+                var args = new PropertyChangedEventArgs(nameof(CantidadRegistros));
+                PropertyChanged?.Invoke(this, args);
+
+            }
+        }
 
         public Command CrearCirculo { get; }
         public Command Abrir { get; }
